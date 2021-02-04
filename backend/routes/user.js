6be71ad07cb373d669f2fb7030196c37b41545db
Samuel Chiche <samuel.chiche24@gmail.com>
2021-02-04 14:43:26 +0100

    Setting routes for user's CRUD

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,5 +5,9 @@ const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
+router.get('/:id', userCtrl.findById);
+router.get('/', userCtrl.findAll);
+router.put('/:id',userCtrl.updateOne);
+router.delete('/', userCtrl.deleteOne);
 
 module.exports = router;
