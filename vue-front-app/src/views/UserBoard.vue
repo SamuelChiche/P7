e2748a974f5401d1fad2167087b4fb70eab5f03a
Samuel Chiche <samuel.chiche24@gmail.com>
2021-02-08 16:57:22 +0100

    Userboard page to test requireAuth method

---
+++ vue-front-app/src/views/UserBoard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="userboard">
+        <h1>This is protected</h1>
+    </div>
+</template>
+<script>
+export default {
+    name : "userboard",
+    component : {
+
+    },
+}
+</script>
