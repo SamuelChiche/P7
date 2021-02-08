68c4619a2a780f334d1858493fe5dff48f16964f
Samuel Chiche <samuel.chiche24@gmail.com>
2021-02-08 16:45:27 +0100

    store import to router + handling unauthorized access cases

--- vue-front-app/src/router/index.js
+++ vue-front-app/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store/store'
 import Home from "../views/Home.vue"
 
 Vue.use(VueRouter)
@@ -13,12 +14,20 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import('../views/Login.vue')
+    component:() => import('../views/Login.vue'),
   },
   {
     path: '/signup',
     name: 'Signup',
-    component: () => import('../views/Signup.vue')
+    component: () => import('../views/Signup.vue'),
+  },
+  {
+    path: '/userboard',
+    name: 'userboard',
+    meta : {
+      requiresAuth: true
+    },
+    component: () => import('../views/UserBoard.vue'),
   }
 ]
 
@@ -26,6 +35,19 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)){
+    if(store.getters.isLoggedIn == true){
+      next()
+      return
+    } 
+      next({path: '/login'})
+    
+  } else {
+    next()
+  } 
 })
 
 export default router
