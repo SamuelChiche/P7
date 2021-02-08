818094fdaa0edd7796c9c106561c267f2fd8983d
Samuel Chiche <samuel.chiche24@gmail.com>
2021-02-08 17:04:06 +0100

    Auth VueX mutations

--- vue-front-app/src/store/store.js
+++ vue-front-app/src/store/store.js
@@ -11,7 +11,21 @@ export default new Vuex.Store({
         user: {}
     },
     mutations: {
-      
+        auth_request(state){
+            state.status = 'loading'
+        },
+        auth_success(state, token, user){
+            state.status = 'success'
+            state.token = token
+            state.user = user
+        },
+        auth_error(state){
+            state.status = 'error'
+        },
+        logout(state){
+            state.status = ''
+            state.token = ''
+        },
     },
     actions : {
         login({commit}, user){
@@ -63,7 +77,7 @@ export default new Vuex.Store({
         }        
     },
     getters : {
-        
+    
     }
 })
 
