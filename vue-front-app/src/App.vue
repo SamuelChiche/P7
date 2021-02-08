a8acaf13dba9d94967f41040ee4ce378b17d24cd
Samuel Chiche <samuel.chiche24@gmail.com>
2021-02-08 17:08:51 +0100

    Import isLoggedIn state and logout methods to the App + handling expired token cases

--- vue-front-app/src/App.vue
+++ vue-front-app/src/App.vue
@@ -2,11 +2,9 @@
   <div id="app">
     <Header/>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/signup">Sign-up</router-link>
-    </div>
+      <router-link to="/">Home</router-link> | <span v-if="isLoggedIn"> <a @click="logout">Logout</a></span>
     <router-view/>
+    </div>
   </div>
 </template>
 
@@ -17,6 +15,25 @@ export default {
   name: 'app',
   components: {
     Header
+  },
+  computed : {
+    isLoggedIn : function(){ return this.$store.getters.isLoggedIn }
+  },
+  methods : {
+    logout: function(){
+      this.$store.dispatch('logout')
+        .then(() => this.$router.push('/login'))
+    }
+  },
+  created: function () {
+    this.$http.interceptors.response.use(undefined, function (err) {
+      return new Promise(function () {
+        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+          this.$store.dispatch('logout')
+        }
+        throw err;
+      });
+    });
   }
 }
 </script>
