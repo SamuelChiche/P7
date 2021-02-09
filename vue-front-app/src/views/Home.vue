6c44f52d3787dc697368246d870cddcd549877a8
Samuel Chiche <samuel.chiche24@gmail.com>
2021-02-09 04:46:13 +0100

    Home signup and login nav features

--- vue-front-app/src/views/Home.vue
+++ vue-front-app/src/views/Home.vue
@@ -1,13 +1,30 @@
 <template>
-    <div class="home">
-        
-    </div>
+  <div class="home text-center" id="home">
+    <main role="main">
+      <h1 class="heading">Lorem, ipsum.</h1>
+      <p class="lead">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam dolorem omnis molestias dolorum numquam consequuntur a, 
+        quaerat minus in, hic quas, delectus 
+      </p>
+      <p>
+        <button class="btn btn-lg btn-secondary" @click="$router.push('/signup')" id="signup_button">S'inscrire</button>
+      </p>
+      <div>
+          Déjà inscrit ? <router-link to="/login">Se connecter</router-link>
+      </div>
+    </main>
+  </div>
 </template>
 <script>
 export default {
-    name : "Home",
-    components : {
+  name: "Home",
+  components: {},
 
-    },
-}
+};
 </script>
+<style lang="scss">
+#home{
+    margin: 100px 500px 0px 500px;
+
+}
+</style>
