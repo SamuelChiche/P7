40816c9ec03ef87566e13cf859bf69e1e1cf8654
Samuel Chiche <samuel.chiche24@gmail.com>
2021-02-12 08:20:10 +0100

    JWT auth middleware

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const authToken = require('../middleware/auth')
      
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.get('/:id', userCtrl.getOneUser);
-router.get('/', userCtrl.getAllUsers);
+router.get('/', authToken, userCtrl.getAllUsers);
 router.put('/:id',userCtrl.updateOneUser);
-router.delete('/', userCtrl.deleteOneUser);
+router.delete('/:id', userCtrl.deleteOneUser);
 
 module.exports = router;
