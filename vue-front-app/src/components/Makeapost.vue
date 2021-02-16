674d153b376a09121b7fdcd0ce2dbbac3ef09f88
Samuel Chiche <samuel.chiche24@gmail.com>
2021-02-16 08:00:42 +0100

    Make a post component first build

---
+++ vue-front-app/src/components/Makeapost.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card gedf-card">
+    <div class="card-header text-center">
+      Post
+    </div>
+    <div class="card-body">
+      <div class="tab-content" id="myTabContent">
+        <div
+          class="tab-pane fade show active"
+          id="posts"
+          role="tabpanel"
+          aria-labelledby="posts-tab"
+        >
+          <div class="form-group">
+            <label class="sr-only" for="message">post</label>
+            <textarea
+              class="form-control"
+              id="message"
+              rows="3"
+              placeholder="What are you thinking?"
+            ></textarea>
+          </div>
+        </div>
+      </div>
+      <div class="btn-toolbar float-right">
+        <div class="btn-group">
+          <button type="submit" class="btn btn-primary">Partager</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Makeapost",
+};
+</script>
