989634578d3016204778e002a309522f49ff305a
Samuel Chiche <samuel.chiche24@gmail.com>
2021-02-23 13:47:47 +0100

    Implementing vote routes first build

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -8,5 +8,7 @@ router.post('/', authToken, postCtrl.create);
 router.get('/', authToken, postCtrl.getAllPosts);
 router.get('/user/:id', authToken , postCtrl.getPostsFromUser);
 router.delete('/:id', authToken , postCtrl.deleteOne);
-
+router.post('/:id/like', postCtrl.vote);
+router.post('/uvote/:id', postCtrl.userUpvote)
+router.post('/dvote/:id', postCtrl.userDownote)
 module.exports = router;
