373e9bb652eff2eae92615acc29531d6f7dd1044
Samuel Chiche <samuel.chiche24@gmail.com>
2021-03-01 01:52:49 +0100

    Userservices delete method

--- vue-front-app/src/services/UserServices.js
+++ vue-front-app/src/services/UserServices.js
@@ -4,6 +4,9 @@ class UserServices {
     getOne(id){
         return axios.get("http://localhost:3000/user/" + id)
     }
+    deleteOne(id){
+        return axios.delete("http://localhost:3000/user/" + id)
+    }
 }
 
 export default new UserServices
