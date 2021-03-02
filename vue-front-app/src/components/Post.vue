5fc3c53cd35ac187cd24a4392c78ee6fb647f074
Samuel Chiche <samuel.chiche24@gmail.com>
2021-03-02 09:03:36 +0100

    Now post compenent can manage comments

--- vue-front-app/src/components/Post.vue
+++ vue-front-app/src/components/Post.vue
@@ -68,26 +68,70 @@
         <p class="card-text">
           {{ post.text }}
         </p>
-        <img :src="post.image" height="400px" width="800px" v-if="post.image != undefined">
+        <img
+          :src="post.image"
+          height="400px"
+          width="800px"
+          v-if="post.image != undefined"
+        />
       </div>
       <div class="card-footer">
-
-    </div>
+        <form>
+          <textarea
+            class="form-control"
+            id="message"
+            rows="1"
+            placeholder="What are you thinking?"
+            v-model="comment_text"
+          ></textarea>
+          <button type="submit" @click="createComment(post.post_id)">
+            Commenter
+          </button>
+        </form>
+        <div
+          v-for="(comment, post_id) in comments_list.filter((comment) => {
+            return comment.post_id == post.post_id;
+          })"
+          v-bind:key="post_id"
+        >
+          <div>
+            {{
+              users_list
+                .map((user) => {
+                  if (user.id === comment.user_id) return user.name;
+                })
+                .join("")
+            }}
+          </div>
+          <div>
+            {{ comment.text }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
 <script>
 import PostServices from "../services/PostServices";
+import CommentServices from "../services/CommentServices";
+import UserServices from "../services/UserServices";
+
 export default {
   name: "Post",
   components: {},
   data() {
     return {
       posts_list: [],
+      comment_text: "",
+      comments_list: [],
+      users_list: [],
     };
   },
-  mounted() {
+  created() {
     this.getAllPosts();
+    this.getAllComments();
+    this.getAllUsers();
   },
   methods: {
     getAllPosts() {
@@ -98,9 +142,30 @@ export default {
         })
         .catch((err) => err);
     },
+    getAllComments() {
+      CommentServices.getAll().then((res) => {
+        this.comments_list = res.data;
+        console.log(this.comments_list);
+      });
+    },
+    getAllUsers() {
+      UserServices.getAll().then((res) => {
+        this.users_list = res.data;
+        console.log(this.users_list);
+      });
+    },
+    createComment(post_id) {
+      let user_id = JSON.parse(this.$store.state.user).id;
+      let text = this.comment_text;
+      let data = {
+        user_id,
+        post_id,
+        text,
+      };
+      CommentServices.create(data);
+    },
     deletePost(id) {
-      PostServices.delete(id)
-        .then(window.location.reload());
+      PostServices.delete(id).then(window.location.reload());
     },
   },
   computed: {},
