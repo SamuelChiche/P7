57e0ba659636d8e063040322f98c029591b37f7e
Samuel Chiche <samuel.chiche24@gmail.com>
2021-03-02 08:13:31 +0100

    Fixing token expiration handler

--- vue-front-app/src/App.vue
+++ vue-front-app/src/App.vue
@@ -19,7 +19,7 @@ export default {
   created: function () {
     this.$http.interceptors.response.use(undefined, function (err) {
       return new Promise(function () {
-        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+        if (err.resonse.status === 401 && err.config && !err.config.__isRetryRequest) {
           this.$store.dispatch('logout')
         }
         throw err;
