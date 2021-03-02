586dae502943eb9d9f0927440f32e6db9abe0252
Samuel Chiche <samuel.chiche24@gmail.com>
2021-03-02 08:12:49 +0100

    CommentServices http methods

---
+++ vue-front-app/src/services/CommentServices.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+class CommentServices {
+    getAll(){
+        return axios.get("http://localhost:3000/comment")
+    }
+
+    create(data){
+        return axios.post("http://localhost:3000/comment", data)
+    }
+
+    getOne(id){
+        return axios.get("http://localhost:3000/comment/" + id)
+    }
+
+    edit(){
+        return axios.get("http://localhost:3000/post")
+    }
+
+    delete(id){
+        return axios.delete("http://localhost:3000/post/" + id)
+    }
+
+    getFromPost(id){
+        return axios.get("http://localhost:3000/comment/post/" + id)
+    }
+}
+
+export default new CommentServices
